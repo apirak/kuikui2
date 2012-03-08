94f8f160508d05521e9969e4ae9a33daeaa80027
Apirak Panatkool <apirak@apirak.com>
2012-03-08 15:11:18 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuikui::Application.config.secret_token = '4358506f0ccae10d33cf955c9a4f242269d7c467f268bd433c5ba62833553b765ab332db1e81d29f289a6daba3fcc5dc4752b00c7008757f84810fc65f6c6d12'
